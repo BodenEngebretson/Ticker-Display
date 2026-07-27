2b1388411087005b0364d2b6967f8ea1de7f3a64
BodenEngebretson <bodenarnold2005@gmail.com>
2026-07-27 12:29:38 -0500

    Learning c++ through Programming Principles and Practice in c++ 3rd Edition by the creator of c++

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,41 @@
 #include <iostream>
 
-int main() { std::cout << "Hello, Ticker Display!\n"; }
+inline void keep_window_open() {
+  char ch;
+  std::cin >> ch;
+}
+
+int main() {
+
+  int Bodens_Age = 20;
+
+  std::cout << "what is your name and age?:\n";
+
+  std::string name = " ";
+  int age = 0;
+
+  std::cin >> name >> age;
+
+  switch (age) {
+  case 20:
+    if (name == "Boden")
+      std::cout << "Hello Admin!\n";
+    else
+      break;
+  default:
+    std::cout << "Hello, " << name << "\nYour age is: " << age << std::endl;
+  }
+
+  int i = 0;
+
+  while (i < 26) {
+
+    char letter = 'a' + i;
+    int val = 'a' + i;
+
+    std::cout << letter << "\t" << val << std::endl;
+    ++i;
+  }
+
+  return 0;
+}
