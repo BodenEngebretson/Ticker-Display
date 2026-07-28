012825f33c6f0b0644675aa4fd69656880535265
BodenEngebretson <bodenarnold2005@gmail.com>
2026-07-28 00:49:18 -0500

    Added a basic stock class

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,31 @@
 #include <iostream>
+#include <vector>
 
-inline void keep_window_open() {
-  char ch;
-  std::cin >> ch;
-}
+class Stock {
+private:
+  double price;
+  std::string ticker;
+
+public:
+  // Getter and Setter Methods for Stock class
+  void set_price(double new_price) { price = new_price; }
+  void set_ticker(std::string new_ticker) { ticker = new_ticker; }
+  double get_price() { return price; }
+  std::string get_ticker() { return ticker; }
+};
 
 int main() {
 
-  int Bodens_Age = 20;
+  std::vector<Stock> Stocks(0);
+  Stock s1;
 
-  std::cout << "what is your name and age?:\n";
+  s1.set_price(100);
+  s1.set_ticker("APPL");
 
-  std::string name = " ";
-  int age = 0;
+  std::string ticker_s1 = s1.get_ticker();
+  double price_s1 = s1.get_price();
 
-  std::cin >> name >> age;
-
-  switch (age) {
-  case 20:
-    if (name == "Boden")
-      std::cout << "Hello Admin!\n";
-    else
-      break;
-  default:
-    std::cout << "Hello, " << name << "\nYour age is: " << age << std::endl;
-  }
-
-  int i = 0;
-
-  while (i < 26) {
-
-    char letter = 'a' + i;
-    int val = 'a' + i;
-
-    std::cout << letter << "\t" << val << std::endl;
-    ++i;
-  }
+  std::cout << "Ticker: " << ticker_s1 << " Price: " << price_s1 << std::endl;
 
   return 0;
 }
