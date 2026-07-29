55181051f1c2c71899ccc2f936d04497f20f73c8
BodenEngebretson <bodenarnold2005@gmail.com>
2026-07-29 14:01:49 -0500

    Functionality added but some logic errors (getters,setters,addingstocks,list of stocks
    Not everything works in this but want stuff saved

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,64 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 class Stock {
+
 private:
   double price;
   std::string ticker;
 
 public:
   // Getter and Setter Methods for Stock class
-  void set_price(double new_price) { price = new_price; }
-  void set_ticker(std::string new_ticker) { ticker = new_ticker; }
+  void set_price(const double new_price) { price = new_price; }
+  void set_ticker(const std::string &new_ticker) { ticker = new_ticker; }
   double get_price() { return price; }
   std::string get_ticker() { return ticker; }
+  bool operator==(const Stock &other) const { return ticker == other.ticker; }
+
+  friend std::ostream &operator<<(std::ostream &, const Stock &);
+  friend std::istream &operator>>(std::istream &, Stock &);
 };
+
+std::ostream &operator<<(std::ostream &os, const Stock &stock) {
+  os << stock.ticker << ' ' << stock.price;
+  return os;
+}
+
+std::istream &operator>>(std::istream &is, Stock &stock) {
+  is >> stock.ticker >> stock.price;
+  return is;
+}
 
 int main() {
 
-  std::vector<Stock> Stocks(0);
-  Stock s1;
+  std::vector<Stock> stocks;
 
-  s1.set_price(100);
-  s1.set_ticker("APPL");
+  // Intro message for display
+  std::cout << "\n\nHello, Welcome to my Stock Ticker Display!\nWhat would you "
+               "like to "
+               "do?:\n\n1:) Display Stocks\n2:) Add Stock\n\n(Press q to quit)"
+            << std::endl;
 
-  std::string ticker_s1 = s1.get_ticker();
-  double price_s1 = s1.get_price();
+  int input;
+  std::cin >> input;
 
-  std::cout << "Ticker: " << ticker_s1 << " Price: " << price_s1 << std::endl;
+  switch (input) {
+  case 1:
+    for (int i = 0; i < stocks.size(); ++i) {
+      std::cout << stocks[i];
+    }
+  case 2:
+    Stock stock;
+
+    if (!(std::cin >> stock)) {
+      std::cout << "Error: Not a valid input.\n";
+    } else if (std::find(stocks.begin(), stocks.end(), stock) != stocks.end()) {
+      stocks.push_back(stock);
+    } else {
+      std::cout << "Error: Input is a duplicate...\n";
+    }
+  }
 
   return 0;
 }
